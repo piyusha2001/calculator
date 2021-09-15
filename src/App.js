414c02a8435ef115741cf3be243df51ffda0cd09
piyusha2001 <ppiyusha2001@gmail.com>
2021-09-15 17:03:17 +0530

    Syntax for buttons changed by using array of objects

--- src/App.js
+++ src/App.js
@@ -3,39 +3,181 @@ import './App.css';
 import React, { useState } from 'react';
 import "animate.css"
 
+
 function App() {
   
-  const [count, setCount] = useState(" ");
- 
- 
-  const result=(e)=>{
-         if(e.target.name==="="){ 
-         
-           setCount(eval(count.toString()));
-         }
-        
-         else{
-         
-          setCount(count.toString().concat(e.target.name));
-          return;
-          
-              }
-     
-    }
-    const clear=(e)=>{
-      setCount(" ")
-    }  
-    const back=(e)=>{
-      setCount(count.slice(0,-1))
-    }
+  const [count, setCount] = useState(0);
 
-    const handleKey=(e)=>{
-      if(e.keyCode===Number(e.target.id)){
-         result(e.key);
-      }
-      
-    }
- 
+  const clear=(e)=>{
+
+  }
+
+  const handleKey=(e)=>{
+
+  }
+  
+  const handleClick=(e)=>{
+
+  }
+
+  const back=(e)=>{
+
+  }
+
+  const result=(e)=>{
+
+    
+  }
+
+  const buttons =[{
+     className: 'item1 button',
+     id:'Esc',
+     name:'Esc',
+     onclick:{clear},
+     onkeypress:{handleKey},
+     children:'AC'
+  },
+  {
+    className: 'item2 button',
+    id:'backspace',
+    name:'backspace',
+    onclick:{back},
+    onkeypress:{handleKey},
+    children:'≪'
+  },
+  {
+    className: 'item3 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'÷'
+  },
+  {
+    className: 'item4 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'7'
+  },
+  {
+    className: 'item5 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'8'
+  },
+  {
+    className: 'item6 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'9'
+  },
+  {
+    className: 'item7 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'+'
+  },
+  {
+    className: 'item8 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'4'
+  },
+  {
+    className: 'item9 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'5'
+  },
+  {
+    className: 'item10 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'6'
+  },
+  {
+    className: 'item11 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'-'
+  },
+  {
+    className: 'item12 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'1'
+  },
+  {
+    className: 'item13 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'2'
+  },
+  {
+    className: 'item14 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'3'
+  },
+  {
+    className: 'item15 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'×'
+  },
+  {
+    className: 'item16 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'0'
+  },
+  {
+    className: 'item17 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{handleClick},
+    onkeypress:{handleKey},
+    children:'.'
+  },
+  {
+    className: 'item18 button',
+    id:'Esc',
+    name:'Esc',
+    onclick:{result},
+    onkeypress:{handleKey},
+    children:'='
+  },
+
+]
+
+
+
   
   
     
@@ -49,29 +191,17 @@ function App() {
         <input className="box" type="text" value={count}/>
       </form>
        <div className="keys">
-        <button className="item1 button" name="Esc" id="Esc" onClick={clear} onKeyPress={handleKey}>AC</button>
-        <button className="item2 button" name="backspace" id="backspace " onClick={back} onKeyPress={handleKey}> &#8810;</button>
-        <button className="item3 button" name="/" id=" /" onClick={result} onKeyPress={handleKey}>&divide;</button>
-        <button className="item4 button" name="7"  id="7" onClick={result} onKeyPress={handleKey} >7</button>
-        <button className="item5 button" name="8"  id="104" onClick={result} onKeyPress={handleKey}>8</button>
-        <button className="item6 button" name="9"  id="105" onClick={result} onKeyPress={handleKey}>9</button>
-        <button className="item7 button" name="+"  id="107" onClick={result} onKeyPress={handleKey}>+</button>
-        <button className="item8 button" name="4"  id="100" onClick={result} onKeyPress={handleKey}>4</button>
-        <button className="item9 button" name="5"  id="101" onClick={result} onKeyPress={handleKey}>5</button>
-        <button className="item10 button" name="6"  id="102" onClick={result} onKeyPress={handleKey}>6</button>
-        <button className="item11 button" name="-"  id="109" onClick={result} onKeyPress={handleKey}>&minus;</button>
-        <button className="item12 button" name="1"  id="97" onClick={result} onKeyPress={handleKey}>1</button>
-        <button className="item13 button" name="2" id="98"  onClick={result} onKeyPress={handleKey}>2</button>
-        <button className="item14 button" name="3" id="99"  onClick={result} onKeyPress={handleKey}>3</button>
-        <button className="item15 button" name="*" id="88" onClick={result} onKeyPress={handleKey}>&times;</button>
-        <button className="item16 button" name="0" id="96" onClick={result} onKeyPress={handleKey}>0</button>
-        <button className="item17 button" name="."  id="190" onClick={result} onKeyPress={handleKey}>.</button>
-        <button className="item18 button" name="Enter"  id="187" onClick={result} onKeyPress={handleKey}>=</button>
-       
+         {
+          buttons.map(button=>(
+            <button{...button}/>
+
+          ))
+         }
+        
       </div>
 
     </div>
   );
-
-  }
+}
+  
 export default App;
