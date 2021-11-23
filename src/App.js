2ae57a8562c5611c8c1491978a3b37e43c31a2e1
piyusha2001 <ppiyusha2001@gmail.com>
2021-11-23 12:51:13 +0530

    final code

--- src/App.js
+++ src/App.js
@@ -1,165 +1,116 @@
 import "./App.css";
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(" ");
 
   const clear = (e) => {
-    setCount(" ")
-
+    setCount(" ");
   };
-
-  const handleKey = (e) => {};
 
   const handleClick = (e) => {
-       setCount(count.concat(e.target.name));
+    setCount(count.concat(e.target.name));
   };
 
-  const back = (e) => {};
+  const backspace = (e) => {
+    setCount(count.slice(0, count.length-1));
+  };
 
   const result = (e) => {
-    setCount(count.eval(e.target.name))
+    setCount(eval(result));
   };
 
   const buttons = [
     
     {
-      className: "item2 button",
-      
-      name: "backspace",
-     
-      
-      onKeyPress: handleKey,
-      children: "≪",
-    },
-    {
       className: "item3 button",
-     
       name: "/",
-     
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "÷",
     },
     {
       className: "item4 button",
-      
       name: "7",
-    
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "7",
     },
     {
       className: "item5 button",
-     
       name: "8",
-    
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "8",
     },
     {
       className: "item6 button",
-     
       name: "9",
-     
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "9",
     },
     {
       className: "item7 button",
-      
       name: "+",
-      
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "+",
     },
     {
       className: "item8 button",
-      
       name: "4",
-      
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "4",
     },
     {
       className: "item9 button",
-     
       name: "5",
-      
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "5",
     },
     {
       className: "item10 button",
-   
       name: "6",
-
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "6",
     },
     {
       className: "item11 button",
-     
       name: "-",
-   
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "-",
     },
     {
       className: "item12 button",
-      
       name: "1",
-      
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "1",
     },
     {
       className: "item13 button",
-     
       name: "2",
-    
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "2",
     },
     {
       className: "item14 button",
-      
       name: "3",
-     
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "3",
     },
     {
       className: "item15 button",
-      
       name: "x",
-     
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "x",
     },
     {
       className: "item16 button",
-      
       name: "0",
-     
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: "0",
     },
     {
       className: "item17 button",
-     
       name: ".",
-     
-      onKeyPress: handleKey,
+      onClick: handleClick,
       children: ".",
-    },
-    {
-      className: "item18 button",
-     
-      name: "=",
-      onClick: result,
-      onKeyPress: handleKey,
-      children: "=",
     },
   ];
 
@@ -170,13 +121,19 @@ function App() {
         <input className="box" type="text" value={count} />
       </form>
       <div className="keys">
-      <button className="button item1" name="Esc" onClick={clear}>AC</button>
+        <button className="button item1" name="Esc" onClick={clear}>
+          AC
+        </button>
+        <button className="button item2" name="backspace" onClick={backspace}>
+          BACK
+        </button>
+        <button className="button item18" name="=" onClick={result}>
+          =
+        </button>
+
         {buttons.map((button) => (
-          
           <button {...button} onClick={handleClick} />
-         
         ))}
-        
       </div>
     </div>
   );
